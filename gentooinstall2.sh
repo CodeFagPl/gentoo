d17d864170bc72e9097706edd6c60fb4312e10a5
CodeFagPl <162654218+CodeFagPl@users.noreply.github.com>
2024-03-08 04:27:09 +0100

    Update gentooinstall2.sh

--- gentooinstall2.sh
+++ gentooinstall2.sh
@@ -3,41 +3,54 @@ disk=sda
 boot=sda1
 lvm=sda2
 
+#continuation from previous file
 source /etc/profile;
 export PS1="(chroot) $PS1";
+
 #preparing boot partition
 mkdir /efi;
 mount /dev/$boot /efi;
-#Updating repository
+
+#updating repository
 mkdir --parents /etc/portage/repos.conf;
 cp /usr/share/portage/config/repos.conf /etc/portage/repos.conf/gentoo.conf;
 echo "sync-git-verify-commit-signature = yes" >> /etc/portage/repos.conf/gentoo.conf;
 emerge-webrsync;
+
 #adding mirrors
 emerge --verbose --oneshot app-portage/mirrorselect;
 mirrorselect -i -o >> /etc/portage/make.conf;
 emerge --sync;
-#Adding cpu flags
+
+#adding cpu flags
 emerge app-portage/cpuid2cpuflags;
 echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/00cpu-flags;
+
 #updating @world flags
 emerge --ask --verbose --update --deep --newuse @world;
-#Setting Timezone
+
+#setting Timezone
 echo "Europe/Warsaw" > /etc/timezone;
 emerge --config sys-libs/timezone-data;
+
 #updating locale
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen;
 locale-gen;
 echo -e 'LANG="de_DE.UTF-8"\nLC_COLLATE="C.UTF-8"' >> /etc/env.d/02locale;
 env-update && source /etc/profile && export PS1="(chroot) ${PS1}";
+
+
+##Kernel configuration##
 #installing firmware
 echo "sys-kernel/linux-firmware compress-zstd redistributable" > /etc/portage/package.use/sys-kernel;
 emerge sys-kernel/linux-firmware;
 echo "sys-firmware/intel-microcode hostonly" > /etc/portage/package.use/sys-firmware;
 emerge sys-firmware/intel-microcode;
+
 #installing genkernel optional can be commented out and done manually
 echo "sys-kernel/genkernel firmware" >> /etc/portage/package.use/sys-kernel;
 emerge gentoo-sources genkernel cryptsetup lvm2;
+
 #configuring fstab file
 echo -e "LABEL=SWAP	none	sw	defaults	0 0\nLABEL=BOOT		/efi	vfat	noatime		0 2\nLABEL=ROOT		/	xfs	defaults	0 1\nLABEL=HOME		/home	xfs	defaults	0 1\nLABEL=NODE		/node	xfs	defaults	0 1" >> /etc/fstab;
 
@@ -58,12 +71,15 @@ grub-mkconfig -o /boot/grub/grub.cfg;
 ##Finalization##
 #setting password for root
 passwd;
+
 #set hostname edit however you want :3
 echo MoneroChan > /etc/hostname;
+
 #configuring the net
 emerge net-misc/dhcpcd;
 rc-update add dhcpcd default;
 rc-service dhcpcd start;
+
 #here insert the name of your net controller
 net=enp4s0;
 echo 'config_${net}="dhcp"' >> /etc/conf.d/net;
