c9cb03ffd86e9d4dd39dd1246b0d39ec441fe1f7
CodeFagPl <162654218+CodeFagPl@users.noreply.github.com>
2024-11-10 08:45:54 +0000

    Update gentooinstall2.sh

--- gentooinstall2.sh
+++ gentooinstall2.sh
@@ -4,7 +4,7 @@ disk=sda; #disk name
 boot=sda1; #boot partition
 lvm=sda2; #lvm partition
 
-env-update && . /etc/profile; #this command is a continuation from previous file do not bother
+env-update && . /etc/profile;
 
 ##Updating Repository##
 mkdir --parents /etc/portage/repos.conf;
@@ -12,28 +12,28 @@ cp /usr/share/portage/config/repos.conf /etc/portage/repos.conf/gentoo.conf;
 echo "sync-git-verify-commit-signature = yes" >> /etc/portage/repos.conf/gentoo.conf;
 emerge-webrsync;
 
-echo 'GENTOO_MIRRORS="https://mirror.wheel.sk/gentoo"' >> /etc/portage/make.conf;  #adding mirrors
+echo 'GENTOO_MIRRORS="https://mirror.wheel.sk/gentoo"' >> /etc/portage/make.conf;
 emerge --sync;
 
-emerge app-portage/cpuid2cpuflags;  #adding cpu flags
+emerge app-portage/cpuid2cpuflags;
 echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/00cpu-flags;
 
 ##Addinng Binary Repos##
 #emerge app-portage/getuto;
 #getuto;
-#echo 'BINPKG_FORMAT="gpkg"'>> /etc/portage/make.conf;
-#echo 'FEATURES="getbinpkg"'>> /etc/portage/make.conf;
+#echo 'BINPKG_FORMAT="gpkg"
+#FEATURES="getbinpkg"
+#FEATURES="binpkg-request-signature"' >> /etc/portage/make.conf;
 #nano /etc/portage/gnupg/pass;  #check password in /etc/portage/gnupg/pass dir
 #gpg --homedir=/etc/portage/gnupg --edit-key 13EBBDBEDE7A12775DFDB1BABB572E0E2D182910;  #sign -> yes  trust -> 4  save
 #gpg --homedir=/etc/portage/gnupg --check-trustdb;
-#echo 'FEATURES="binpkg-request-signature"'>> /etc/portage/make.conf;
 #emerge --sync;
 #rm -r /etc/portage/binrepos.conf;
 #echo '[binhost]
 #sync-uri = https://mirror.wheel.sk/gentoo/releases/amd64/binpackages/23.0/x86-64/
 #priority = 10' > /etc/portage/binrepos.conf;
 
-emerge -vuDN @world;  #updating @world flags
+emerge -vuDN @world;
 
 ##Setting Timezone##
 echo "Europe/Warsaw" > /etc/timezone; #change to match your timezone
@@ -48,7 +48,7 @@ env-update && . /etc/profile;
 
 
 ##Kernel configuration##
-emerge app-arch/lz4;  #necessary for lz4 compression of kernel modules
+emerge app-arch/lz4;
 emerge sys-kernel/linux-firmware;
 emerge sys-firmware/sof-firmware;
 env-update && . /etc/profile;
@@ -84,7 +84,7 @@ dracut --kver 6.11.7-gentoo;  #change to match kernel version you will download
 echo "sys-boot/grub mount device-mapper" > /etc/portage/package.use/sys-boot;
 emerge grub gentoolkit;
 echo 'GRUB_CMDLINE_LINUX="quiet net.ifnames=0"
-GRUB_ENABLE_CRYPTODISK=y' >> /etc/default/grub;  #you can change your cmdline arguments according to your needs
+GRUB_ENABLE_CRYPTODISK=y' >> /etc/default/grub;
 nano /etc/lvm/lvm.conf;  #Set: allow-discards = 1    
 nano /etc/default/grub;  #check if everything is alright
 grub-install --efi-directory=/boot --bootloader-id=GRUB --recheck;
@@ -93,17 +93,17 @@ nano /boot/grub/grub.cfg;
 
 ##Finalization##
 echo GentooPC > /etc/hostname; #set hostname edit however you want :3
-emerge net-misc/dhcpcd;  #configuring the net, if you do not want dhcp comment it out
-rc-update add dhcpcd default;
-rc-service dhcpcd start;
-net=enp4s0; #here insert the name of your net controller
-beg='config_'"$net";
-end='="dhcp"';
-netconfig="$beg""$end";
-echo "$netconfig" >> /etc/conf.d/net;
-cd /etc/init.d;
-ln -s net.lo net.$net;
-rc-update add net.$net default;
+#emerge net-misc/dhcpcd;  #configuring the net, if you do not want dhcp comment it out
+#rc-update add dhcpcd default;
+#rc-service dhcpcd start;
+#net=enp4s0; #here insert the name of your net controller
+#beg='config_'"$net";
+#end='="dhcp"';
+#netconfig="$beg""$end";
+#echo "$netconfig" >> /etc/conf.d/net;
+#cd /etc/init.d;
+#ln -s net.lo net.$net;
+#rc-update add net.$net default;
 
 ##Installing tools##
 emerge syslog-ng cronie;
